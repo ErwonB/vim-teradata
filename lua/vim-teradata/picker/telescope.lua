2304c4947fc1253603daedb62822807f70f5681f
Erwan Bourre <github@bourre.me>
2026-03-14 08:02:45 +0100

    fix: for telescope picker

--- lua/vim-teradata/picker/telescope.lua
+++ lua/vim-teradata/picker/telescope.lua
@@ -1,22 +1,20 @@
 local M = {}
 
-local conf = require('telescope.config').values
-local finders = require('telescope.finders')
-local pickers = require('telescope.pickers')
-local action_state = require('telescope.actions.state')
-local actions = require('telescope.actions')
-
 function M.grep_queries(queries_dir, on_select)
     require('telescope.builtin').live_grep({
         search_dirs = { queries_dir },
         prompt_title = 'Grep Queries',
-        attach_mappings = function(prompt_bufnr, map)
+        attach_mappings = function(prompt_bufnr, _)
+            local actions = require('telescope.actions')
+            local action_state = require('telescope.actions.state')
             actions.select_default:replace(function()
                 actions.close(prompt_bufnr)
                 local selection = action_state.get_selected_entry()
                 if selection then
                     local filename = vim.fn.fnamemodify(selection.filename, ':t:r')
-                    on_select(filename)
+                    vim.schedule(function()
+                        on_select(filename)
+                    end)
                 end
             end)
             return true
@@ -28,6 +26,12 @@ function M.pick_completion(items, context, opts, on_select)
     local fzf_options = opts.fzf_options or ''
     local is_multi = string.find(fzf_options, '%-%-multi') ~= nil
 
+    local actions = require('telescope.actions')
+    local action_state = require('telescope.actions.state')
+    local finders = require('telescope.finders')
+    local pickers = require('telescope.pickers')
+    local conf = require('telescope.config').values
+
     pickers.new({}, {
         prompt_title = 'SQL Completion',
         finder = finders.new_table({
@@ -35,25 +39,37 @@ function M.pick_completion(items, context, opts, on_select)
         }),
         sorter = conf.generic_sorter({}),
         attach_mappings = function(prompt_bufnr, map)
+            -- Disable multi-select keybinding when not in multi mode
+            if not is_multi then
+                map({ 'i', 'n' }, '<Tab>', function() end)
+            end
+
             actions.select_default:replace(function()
                 local picker = action_state.get_current_picker(prompt_bufnr)
-                local multi_selections = picker:get_multi_selection()
-                
                 local selected = {}
-                if is_multi and #multi_selections > 0 then
-                    for _, selection in ipairs(multi_selections) do
-                        table.insert(selected, selection[1])
+
+                if is_multi then
+                    local multi_selections = picker:get_multi_selection()
+                    if #multi_selections > 0 then
+                        for _, selection in ipairs(multi_selections) do
+                            table.insert(selected, selection[1])
+                        end
                     end
-                else
+                end
+
+                -- Fallback to current item if nothing was multi-selected
+                if #selected == 0 then
                     local selection = action_state.get_selected_entry()
                     if selection then
                         table.insert(selected, selection[1])
                     end
                 end
-                
+
                 actions.close(prompt_bufnr)
                 if #selected > 0 then
-                    on_select(selected, context)
+                    vim.schedule(function()
+                        on_select(selected, context)
+                    end)
                 end
             end)
             return true
